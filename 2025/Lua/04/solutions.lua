436d151892a3db1706c81220a04467074e7aff90
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2025-12-05 09:01:23 +1100

    perf: 2025[lua] 04

--- 2025/Lua/04/solutions.lua
+++ 2025/Lua/04/solutions.lua
@@ -5,50 +5,35 @@ local function part1()
 		table.insert(grid, line)
 	end
 
+	local len = string.len(grid[1])
+	local empty_line = string.rep(".", len)
+
 	for x = 1, #grid do
 		local line = grid[x]
-		local len = string.len(line)
-		local empty_line = string.rep(".", len)
-		for y = 1, len do
-			local count = 0
 
+		for y = 1, len do
 			local char = line:sub(y, y)
+
 			if char ~= "@" then
 				goto continue
 			end
 
-			local start_pos = y
-			if start_pos ~= 1 then
-				start_pos = start_pos - 1
-			end
+			local start_pos = math.max(1, y - 1)
+			local end_pos = math.min(len, y + 1)
 
-			local end_pos = y
-			if end_pos ~= len then
-				end_pos = end_pos + 1
-			end
-
+			local count = 0
 			local top_line = grid[x - 1] or empty_line
-			local top_chars = top_line:sub(start_pos, end_pos)
-			local _, top_count = top_chars:gsub("@", "")
-			count = count + top_count
-
-			local current_chars = line:sub(start_pos, end_pos)
-			local _, current_count = current_chars:gsub("@", "")
-			count = count + current_count
-
-			if count > 4 then
-				goto continue
-			end
-
 			local bottom_line = grid[x + 1] or empty_line
-			local bottom_chars = bottom_line:sub(start_pos, end_pos)
-			local _, bottom_count = bottom_chars:gsub("@", "")
-			count = count + bottom_count
 
-			if count > 4 then
-				goto continue
+			for i = start_pos, end_pos do
+				if top_line:sub(i, i) == "@" then count = count + 1 end
+				if line:sub(i, i) == "@" then count = count + 1 end
+				if bottom_line:sub(i, i) == "@" then count = count + 1 end
+
+				if count > 4 then
+					goto continue
+				end
 			end
-
 			total = total + 1
 
 			::continue::
@@ -65,52 +50,37 @@ local function part2()
 		table.insert(grid, line)
 	end
 
+	local len = string.len(grid[1])
+	local empty_line = string.rep(".", len)
 
 	local diff = true
 	while diff do
 		local diff_total = total
+
 		for x = 1, #grid do
 			local line = grid[x]
-			local len = string.len(line)
-			local empty_line = string.rep(".", len)
-			for y = 1, len do
-				local count = 0
 
+			for y = 1, len do
 				local char = line:sub(y, y)
 				if char ~= "@" then
 					goto continue
 				end
 
-				local start_pos = y
-				if start_pos ~= 1 then
-					start_pos = start_pos - 1
-				end
+				local start_pos = math.max(1, y - 1)
+				local end_pos = math.min(len, y + 1)
 
-				local end_pos = y
-				if end_pos ~= len then
-					end_pos = end_pos + 1
-				end
-
+				local count = 0
 				local top_line = grid[x - 1] or empty_line
-				local top_chars = top_line:sub(start_pos, end_pos)
-				local _, top_count = top_chars:gsub("@", "")
-				count = count + top_count
-
-				local current_chars = line:sub(start_pos, end_pos)
-				local _, current_count = current_chars:gsub("@", "")
-				count = count + current_count
-
-				if count > 4 then
-					goto continue
-				end
-
 				local bottom_line = grid[x + 1] or empty_line
-				local bottom_chars = bottom_line:sub(start_pos, end_pos)
-				local _, bottom_count = bottom_chars:gsub("@", "")
-				count = count + bottom_count
 
-				if count > 4 then
-					goto continue
+				for i = start_pos, end_pos do
+					if top_line:sub(i, i) == "@" then count = count + 1 end
+					if line:sub(i, i) == "@" then count = count + 1 end
+					if bottom_line:sub(i, i) == "@" then count = count + 1 end
+
+					if count > 4 then
+						goto continue
+					end
 				end
 
 				total = total + 1
